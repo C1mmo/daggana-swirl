d381274a5baf7841443940fdce48a0141ec2ebc4
Raatikaine@gmail.com <Raatikaine@gmail.com@77d15785-523f-4c91-1099-313c9f143321>
2011-04-07 16:17:19 +0000

    First code revision

---
+++ Daggana-Swirl/Daggana.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   Daggana.cpp
+ * Author: miitri
+ * 
+ * Created on April 7, 2011, 6:46 PM
+ */
+
+#include "Daggana.h"
+
+Daggana::Daggana()
+{
+}
+
+Daggana::Daggana(const Daggana& orig)
+{
+}
+
+Daggana::~Daggana()
+{
+}
+
---
+++ Daggana-Swirl/Daggana.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   Daggana.h
+ * Author: miitri
+ *
+ * Created on April 7, 2011, 6:46 PM
+ */
+
+#ifndef DAGGANA_H
+#define	DAGGANA_H
+
+class Daggana
+{
+public:
+	Daggana();
+	Daggana(const Daggana& orig);
+	virtual ~Daggana();
+private:
+
+};
+
+#endif	/* DAGGANA_H */
+
