75c71faa2156b33edf5e19ff04950ed004424f40
Raatikaine@gmail.com <Raatikaine@gmail.com@77d15785-523f-4c91-1099-313c9f143321>
2011-04-08 15:05:14 +0000

    BoardView added

---
+++ Daggana-Swirl/BoardView.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   BoardView.cpp
+ * Author: miitri
+ * 
+ * Created on April 8, 2011, 5:49 PM
+ */
+
+#include "BoardView.h"
+
+BoardView::BoardView()
+{
+}
+
+BoardView::BoardView(const BoardView& orig)
+{
+}
+
+BoardView::~BoardView()
+{
+}
+
---
+++ Daggana-Swirl/BoardView.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   BoardView.h
+ * Author: Miitri
+ *
+ * Created on April 8, 2011, 5:49 PM
+ */
+
+#ifndef BOARDVIEW_H
+#define	BOARDVIEW_H
+
+class BoardView
+{
+public:
+	BoardView();
+	BoardView(const BoardView& orig);
+	virtual ~BoardView();
+private:
+
+};
+
+#endif	/* BOARDVIEW_H */
+
